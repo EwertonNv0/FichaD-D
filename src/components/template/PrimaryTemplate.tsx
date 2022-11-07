1fcd30671a065c0e687ab6dee73f68f4f4571e14
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-07 09:56:02 -0300

    Update PrimaryTemplate.tsx

--- src/components/template/PrimaryTemplate.tsx
+++ src/components/template/PrimaryTemplate.tsx
@@ -1,9 +1,9 @@
-import { Box, HStack, VStack } from 'native-base';
+import { VStack } from 'native-base';
 
-export function PrimaryTemplate(props) {
+export function PrimaryTemplate( props ) {
     return (
         <VStack bg='gray.600' flex={1}>
-            <VStack w='full' flex={1} pt={9} px={2}>
+            <VStack w='full' flex={1} pt={9}>
                 {props.children}
             </VStack>
         </VStack>
