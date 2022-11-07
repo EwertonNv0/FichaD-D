af54fffe43efe766c9f3a705ed6f6dc26a250951
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-07 09:55:57 -0300

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Center, Spinner } from "native-base";
 export function Loading() {
   return (
     <Center flex={1}>
-        <Spinner color='gray.100'/>
+        <Spinner color='primary.700'/>
     </Center>
   );
 }
