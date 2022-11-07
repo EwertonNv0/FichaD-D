12672fd7c66d6150c6a41358af3aee2541a68694
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-07 09:56:06 -0300

    Update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,30 @@
-import { Box, HStack, IconButton, useTheme } from 'native-base';
-import { List } from 'phosphor-react-native';
+import { Heading, VStack } from 'native-base';
+import { CharacterItem } from '../components/CharacterItem';
+import { NewCharacter } from '../components/NewCharacter';
+import { ScrollTemplate } from '../components/ScrollTemplate';
 import { PrimaryTemplate } from '../components/template/PrimaryTemplate';
+import { WhiteTemplate } from '../components/template/WhiteTemplate';
 
 export function Home() {
-
-    const { colors } = useTheme();
-
     return (
         <PrimaryTemplate>
-            <HStack>
-                <Box>
-                    
-                </Box>
-            </HStack>
+            <VStack flex={1}>
+                <Heading color='white' alignSelf='center' fontSize='2xl' my={2}>
+                    Seus Personagens
+                </Heading>
+
+                <WhiteTemplate>
+                    <VStack h='90%'>
+                        <ScrollTemplate>
+                            <CharacterItem />
+                            <CharacterItem />
+                            <CharacterItem />
+                        </ScrollTemplate>
+                    </VStack>
+
+                    <NewCharacter />
+                </WhiteTemplate>
+            </VStack>
         </PrimaryTemplate>
     );
 }
