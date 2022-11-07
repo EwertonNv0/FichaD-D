f9a96d0e560abd057e11248385421b0104ce5106
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-07 11:10:04 -0300

    Create CharacterScreen.tsx

---
+++ src/screens/CharacterScreen.tsx
@@ -0,0 +1,10 @@
+import { VStack } from 'native-base';
+import { PrimaryTemplate } from '../components/template/PrimaryTemplate';
+
+export function CharacterScreen() {
+  return (
+    <PrimaryTemplate>
+        
+    </PrimaryTemplate>
+  );
+}
