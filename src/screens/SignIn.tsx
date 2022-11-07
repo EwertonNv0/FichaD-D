e88f8407096eb32b707bad270372c7dd6e0cffd5
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-07 09:56:09 -0300

    Update SignIn.tsx

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,59 +3,62 @@ import { color } from 'native-base/lib/typescript/theme/styled-system';
 import { Envelope, Key } from 'phosphor-react-native';
 import { BasicImput } from '../components/BasicImput';
 import { LoginRegister } from '../components/LoginRegister';
+import { PrimaryTemplate } from '../components/template/PrimaryTemplate';
 
 export function SignIn() {
 
   const { colors } = useTheme();
 
   return (
-    <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
-      <Box>
-        <Image
-          source={require('../assets/logo.png')}
-          alt='Torre do Mago'
-          w={32}
-          h={32}
-        />
-      </Box>
+    <PrimaryTemplate>
+      <VStack alignItems='center' px={8} pt={24}>
+        <Box>
+          <Image
+            source={require('../assets/logo.png')}
+            alt='Torre do Mago'
+            w={24}
+            h={24}
+          />
+        </Box>
 
-      <Heading color='white' fontSize='xl'>
-        Acesse sua conta
-      </Heading>
+        <Heading color='white' fontSize='xl' mt={2}>
+          Acesse sua conta
+        </Heading>
 
-      <Box mt={5}>
-        <BasicImput
-          placeholder='Email'
-          InputLeftElement={<Icon as={<Envelope color={colors.gray['200']} />} ml={4}/>}
-          isRequired
-        />
+        <Box mt={5}>
+          <BasicImput
+            placeholder='Email'
+            InputLeftElement={<Icon as={<Envelope color={colors.gray['200']} />} ml={4} />}
+            isRequired
+          />
 
-        <BasicImput
-          my={5}
-          placeholder='Senha'
-          InputLeftElement={<Icon as={<Key color={colors.gray['200']} />} ml={4}/>}
-          secureTextEntry
-          isRequired
-        />
+          <BasicImput
+            my={5}
+            placeholder='Senha'
+            InputLeftElement={<Icon as={<Key color={colors.gray['200']} />} ml={4} />}
+            secureTextEntry
+            isRequired
+          />
 
-        <LoginRegister
-          title="Entrar"
-        />
+          <LoginRegister
+            title="Entrar"
+          />
 
-        <Button
-          mt={5}
-          py={0}
-          borderWidth={0}
-          bg='transparent'
-          _pressed={{
-            bg: 'gray.400'
-          }}
-        >
-          <Text fontSize='sm' color='gray.200'>
-            Não possuo conta.
-          </Text>
-        </Button>
-      </Box>
-    </VStack>
+          <Button
+            mt={5}
+            py={0}
+            borderWidth={0}
+            bg='transparent'
+            _pressed={{
+              bg: 'gray.400'
+            }}
+          >
+            <Text fontSize='sm' color='gray.200'>
+              Não possuo conta.
+            </Text>
+          </Button>
+        </Box>
+      </VStack>
+    </PrimaryTemplate>
   );
 }
