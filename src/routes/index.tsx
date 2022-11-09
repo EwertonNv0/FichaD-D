5d5a1db7dc488b1025671f221caffeed850e7005
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-08 22:02:17 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './app.routes';
 
-export function Routs() {
+export function Routes() {
     return (
         <NavigationContainer>
             <AppRoutes />
