1402216d2c9899a8c84d6be93fbbf9495389391c
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-08 22:02:19 -0300

    Create ScrollTemplate.tsx

---
+++ src/components/template/ScrollTemplate.tsx
@@ -0,0 +1,13 @@
+import { ScrollView } from 'native-base';
+
+export function ScrollTemplate(props, {...rest}) {
+    return (
+        <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 50 }}
+            {...rest}
+        >
+            {props.children}
+        </ScrollView>
+    );
+}
