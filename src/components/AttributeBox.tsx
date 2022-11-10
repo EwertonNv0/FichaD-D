2697bb39d3cd33309328adf864ae5af22b615a5f
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-10 01:19:12 -0300

    Update AttributeBox.tsx

--- src/components/AttributeBox.tsx
+++ src/components/AttributeBox.tsx
@@ -1,8 +1,8 @@
-import { Box, HStack, Text } from 'native-base';
+import { Box, HStack, Input, Text } from 'native-base';
 
 export function AttributeBox() {
     return (
-        <Box mb={4}>
+        <Box>
             <HStack
                 borderWidth={2}
                 borderColor='black'
@@ -33,13 +33,23 @@ export function AttributeBox() {
                 w={8}
                 h={6}
             >
-                <Text
+                {/* <Text
                     color='black'
                     fontSize='md'
                     alignSelf='center'
                 >
                     20
-                </Text>
+                </Text> */}
+                <Input
+                    fontSize='md'
+                    px={0}
+                    py={0}
+                    w='full'
+                    textAlign='center'
+                    keyboardType='numeric'
+                    maxLength={2}
+                    borderWidth={0}
+                />
             </HStack>
         </Box>
     );
