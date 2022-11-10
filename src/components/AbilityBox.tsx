a191045ca1cec2620978e48c28dd8c49d41a3ef9
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-10 19:42:12 -0300

    Update AbilityBox.tsx

--- src/components/AbilityBox.tsx
+++ src/components/AbilityBox.tsx
@@ -1,12 +1,36 @@
-import { Button, Checkbox, HStack, Text, useTheme } from 'native-base';
+import { Button, Checkbox, HStack, Text } from 'native-base';
+import { useState } from 'react';
 
-export function AbilityBox({ title }) {
+
+export function AbilityBox({ title, atributo }) {
+
+    const [ bonus, setBonus ] = useState(false);
+    const modificador = () => {
+        const intMod = parseInt(atributo)
+        if (isNaN(intMod)) {
+            return ''
+        }
+        else {
+            const mod = Math.floor((intMod - 10) / 2) * (bonus ? 2 : 1)
+            return mod <= 0 ? mod : `+${mod}`
+        }
+    }
+
     return (
         <HStack w='full' alignItems='center'>
-            <Checkbox borderRadius='full' value="proficiency" colorScheme='red' accessibilityLabel='proficiency' />
+            <Checkbox
+                borderRadius='full'
+                value='proficiency'
+                colorScheme='red'
+                accessibilityLabel='proficiency'
+                isChecked={bonus}
+                onChange={() => {setBonus(!bonus)}}
+            />
+
             <Text mx={2} fontSize='sm'>
-                +20
+                {modificador()}
             </Text>
+
             <Button
                 px={1}
                 py={0}
