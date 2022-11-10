bdb206dd3778319227064330466a51ff172ef657
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-10 01:19:19 -0300

    Update CharacterScreen.tsx

--- src/screens/CharacterScreen.tsx
+++ src/screens/CharacterScreen.tsx
@@ -1,7 +1,7 @@
-import { Button, Center, Heading, HStack, Icon, IconButton, Text, VStack } from 'native-base';
-import { color } from 'native-base/lib/typescript/theme/styled-system';
+import { Box, Button, Center, Checkbox, HStack, Icon, Input, Text, VStack } from 'native-base';
 import { PencilLine } from 'phosphor-react-native';
 import { AttributeBox } from '../components/AttributeBox';
+import { AttributeContainer } from '../components/AttributeContainer';
 import { Label } from '../components/Label';
 import { PrimaryTemplate } from '../components/template/PrimaryTemplate';
 import { ScrollTemplate } from '../components/template/ScrollTemplate';
@@ -10,43 +10,98 @@ import { WhiteTemplate } from '../components/template/WhiteTemplate';
 export function CharacterScreen() {
   return (
     <PrimaryTemplate>
-      <HStack>
+      <HStack
+        justifyContent='center'
+        alignItems='center'
+      >
         <Text fontWeight='bold' color='white' alignSelf='center' fontSize='2xl' my={2}>
           Nome do Personagem
         </Text>
 
-        <Button
-          px={0}
-          // py={0}
-        >
-          <Icon as={<PencilLine color='white' />} />
-        </Button>
+        <Box ml={2}>
+          <Button
+            px={2}
+            py={2}
+            bg='transparent'
+            borderRadius='full'
+            _pressed={{
+              backgroundColor: 'gray.400'
+            }}
+          >
+            <Icon as={<PencilLine color='white' />} />
+          </Button>
+        </Box>
       </HStack>
 
       <WhiteTemplate>
         <ScrollTemplate>
-          <HStack>
-            <VStack>
-              <Center>
-                <Label title='Força' />
-                <AttributeBox />
-                <Label title='Destreza' />
-                <AttributeBox />
-                <Label title='Constituição' />
-                <AttributeBox />
-                <Label title='Inteligência' />
-                <AttributeBox />
-                <Label title='Sabedoria' />
-                <AttributeBox />
-                <Label title='Carisma' />
-                <AttributeBox />
-              </Center>
-            </VStack>
+          <VStack w='full'>
+            <HStack justifyContent='space-around' mb={4}>
+              <AttributeContainer w='25%' alignSelf='flex-start' title='Força' />
 
-            <VStack>
+              <VStack w='70%'>
+                <Checkbox value="testSave">Teste de Resistência</Checkbox>
+                <Checkbox value="atletics">Atletismo</Checkbox>
+              </VStack>
+            </HStack>
 
-            </VStack>
-          </HStack>
+            <HStack justifyContent='space-around' mb={4}>
+              <AttributeContainer w='25%' alignSelf='flex-start' title='Destreza' />
+
+              <VStack w='70%'>
+                <Checkbox value="testSave">Teste de Resistência</Checkbox>
+                <Checkbox value="acrobatics">Acrobacia</Checkbox>
+                <Checkbox value="stealth">Furtividade</Checkbox>
+                <Checkbox value="prestidigitation">Prestidigitação</Checkbox>
+              </VStack>
+            </HStack>
+
+            <HStack justifyContent='space-around' mb={4}>
+              <AttributeContainer w='25%' alignSelf='flex-start' title='Constituição' />
+
+              <VStack w='70%'>
+                <Checkbox value="testSave">Teste de Resistência</Checkbox>
+              </VStack>
+            </HStack>
+
+            <HStack justifyContent='space-around' mb={4}>
+              <AttributeContainer w='25%' alignSelf='flex-start' title='Inteligência' />
+
+              <VStack w='70%'>
+                <Checkbox value="testSave">Teste de Resistência</Checkbox>
+                <Checkbox value="arcanism">Arcanismo</Checkbox>
+                <Checkbox value="history">História</Checkbox>
+                <Checkbox value="investigation">Investigação</Checkbox>
+                <Checkbox value="nature">Natureza</Checkbox>
+                <Checkbox value="religion">Religião</Checkbox>
+              </VStack>
+            </HStack>
+
+            <HStack justifyContent='space-around' mb={4}>
+              <AttributeContainer w='25%' alignSelf='flex-start' title='Sabedoria' />
+
+              <VStack w='70%'>
+                <Checkbox value="testSave">Teste de Resistência</Checkbox>
+                <Checkbox value="animalHandling">Adestrar Animais</Checkbox>
+                <Checkbox value="intuition">Intuição</Checkbox>
+                <Checkbox value="medicin">Medicina</Checkbox>
+                <Checkbox value="perception">Percepção</Checkbox>
+                <Checkbox value="survivor">Sobrevivência</Checkbox>
+              </VStack>
+            </HStack>
+
+            <HStack justifyContent='space-around' mb={4}>
+              <AttributeContainer w='25%' alignSelf='flex-start' title='Carisma' />
+
+              <VStack w='70%'>
+                <Checkbox value="testSave">Teste de Resistência</Checkbox>
+                <Checkbox value="acting">Atuação</Checkbox>
+                <Checkbox value="cheat">Enganação</Checkbox>
+                <Checkbox value="intimidation">Intimidação</Checkbox>
+                <Checkbox value="persiation">Persuasão</Checkbox>
+              </VStack>
+            </HStack>
+          </VStack>
         </ScrollTemplate>
       </WhiteTemplate>
     </PrimaryTemplate>
