802eda9323fc7441b74d210e2c2123b0d1137135
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-11 13:28:51 -0300

    Update Loading.tsx
    ajustando spiner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,12 @@
 import { Center, Spinner } from "native-base";
+import { PrimaryTemplate } from "./template/PrimaryTemplate";
 
 export function Loading() {
   return (
-    <Center flex={1}>
-        <Spinner color='primary.700'/>
-    </Center>
+    <PrimaryTemplate>
+      <Center flex={1}>
+        <Spinner color='primary.700' size='lg' />
+      </Center>
+    </PrimaryTemplate>
   );
 }
