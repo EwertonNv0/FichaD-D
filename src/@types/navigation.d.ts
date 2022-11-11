ad78b3286020ef6f679cc5a6709c35b7698f8d66
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-10 21:03:51 -0300

    Update navigation.d.ts

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -3,7 +3,9 @@ export declare global {
         interface RootParamList {
             home: undefined;
             signIn: undefined;
-            charScreen: undefined
+            charScreen: {
+                id: string
+            }
         }
     }
 }
