5ffa65510cd481efd75a3fca802234c41d977380
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-30 13:56:22 -0300

    classes, raças e sub-raças

---
+++ src/utils/Classes.tsx
@@ -0,0 +1,50 @@
+export const classes = [
+    {
+        id: 1,
+        name: 'Mago'
+    },
+    {
+        id: 2,
+        name: 'Barbaro'
+    },
+    {
+        id: 3,
+        name: 'Bardo'
+    },
+    {
+        id: 4,
+        name: 'Bruxo'
+    },
+    {
+        id: 5,
+        name: 'Clérigo'
+    },
+    {
+        id: 6,
+        name: 'Druida'
+    },
+    {
+        id: 7,
+        name: 'Guerreiro'
+    },
+    {
+        id: 8,
+        name: 'Monge'
+    },
+    {
+        id: 9,
+        name: 'Paladino'
+    },
+    {
+        id: 10,
+        name: 'Patrulheiro'
+    },
+    {
+        id: 11,
+        name: 'Ladino'
+    },
+    {
+        id: 12,
+        name: 'Feitiçeiro'
+    },
+]
--- src/utils/Races.tsx
+++ src/utils/Races.tsx
@@ -2,13 +2,125 @@ export const races = [
     {
         id: 1,
         name: 'Humano',
+        subClass: [
+            {
+                id: 1,
+                name: 'Variante'
+            }
+        ]
     },
     {
         id: 2,
-        name: 'Elfo'
+        name: 'Elfo',
+        subClass: [
+            {
+                id: 1,
+                name: 'Alto Elfo'
+            },
+            {
+                id: 2,
+                name: 'Elfo da Floresta'
+            },
+            {
+                id: 3,
+                name: 'Elfo Negro (Drow)'
+            },
+        ]
     },
     {
         id: 3,
-        name: 'Anão'
+        name: 'Anão',
+        subClass: [
+            {
+                id: 1,
+                name: 'Duergar'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Halfling',
+        subClass: [
+            {
+                id: 1,
+                name: 'Pés Leves'
+            },
+            {
+                id: 2,
+                name: 'Robusto'
+            },
+        ]
+    },
+    {
+        id: 5,
+        name: 'Meio-Elfo'
+    },
+    {
+        id: 6,
+        name: 'Meio-Orc'
+    },
+    {
+        id: 7,
+        name: 'Tiefling'
+    },
+    {
+        id: 8,
+        name: 'Draconato',
+        subClass: [
+            {
+                id: 1,
+                name: 'Azul (Elétrico)'
+            },
+            {
+                id: 2,
+                name: 'Branco (Frio)'
+            },
+            {
+                id: 3,
+                name: 'Bronze (Elétrico)'
+            },
+            {
+                id: 4,
+                name: 'Cobre (Ácido)'
+            },
+            {
+                id: 5,
+                name: 'Latão (Fogo)'
+            },
+            {
+                id: 6,
+                name: 'Negro (Ácido)'
+            },
+            {
+                id: 7,
+                name: 'Ouro (Fogo)'
+            },
+            {
+                id: 8,
+                name: 'Prata (Frio)'
+            },
+            {
+                id: 9,
+                name: 'Verde (Veneno)'
+            },
+            {
+                id: 10,
+                name: 'Vermelho (Fogo)'
+            },
+        ]
+    },
+    {
+        id: 9,
+        name: 'Gnomo',
+        subClass: [
+            {
+                id: 1,
+                name: 'Da Floresta'
+            },
+            {
+                id: 2,
+                name: 'Das Rochas'
+            },
+        ]
     },
 ]
