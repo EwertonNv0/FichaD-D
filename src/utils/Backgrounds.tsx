4e04d8ec70ddacac7826813bcc8ccec276fad20d
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-11-30 15:50:31 -0300

    Create Backgrounds.tsx

---
+++ src/utils/Backgrounds.tsx
@@ -0,0 +1,105 @@
+export const backgrounds = [
+    {
+        id: 1,
+        name: 'Acólito',
+    },
+    {
+        id: 2,
+        name: 'Artesão da Guilda',
+        variantDescription: 'Variação',
+        variant: [
+            {
+                id: 1,
+                name: 'Nemnhum'
+            },
+            {
+                id: 2,
+                name: 'Mercador de Guilda'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Artista',
+        variantDescription: 'Variação',
+        variant: [
+            {
+                id: 1,
+                name: 'Nemnhum'
+            },
+            {
+                id: 2,
+                name: 'Gladiador'
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'Criminoso',
+        variantDescription: 'Variação',
+        variant: [
+            {
+                id: 1,
+                name: 'Nemnhum'
+            },
+            {
+                id: 2,
+                name: 'Espião'
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Eremita',
+    },
+    {
+        id: 6,
+        name: 'Forasteiro',
+    },
+    {
+        id: 7,
+        name: 'Herói do Povo',
+    },
+    {
+        id: 8,
+        name: 'Marinheiro',
+        variantDescription: 'Variação',
+        variant: [
+            {
+                id: 1,
+                name: 'Nemnhum'
+            },
+            {
+                id: 2,
+                name: 'Pirata'
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: 'Nobre',
+        variantDescription: 'Variação',
+        variant: [
+            {
+                id: 1,
+                name: 'Nemnhum'
+            },
+            {
+                id: 2,
+                name: 'Cavaleiro'
+            }
+        ]
+    },
+    {
+        id: 10,
+        name: 'Orfão',
+    },
+    {
+        id: 11,
+        name: 'Sábio',
+    },
+    {
+        id: 12,
+        name: 'Soldado',
+    }
+]
