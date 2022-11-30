204f063182e7a3e7f9808bdd97971edd0621e133
ysrael-tavares <ysraeltavares33@gmail.com>
2022-11-30 13:30:17 -0300

    Create Races.tsx

---
+++ src/utils/Races.tsx
@@ -0,0 +1,14 @@
+export const races = [
+    {
+        id: 1,
+        name: 'Humano',
+    },
+    {
+        id: 2,
+        name: 'Elfo'
+    },
+    {
+        id: 3,
+        name: 'Anão'
+    },
+]
