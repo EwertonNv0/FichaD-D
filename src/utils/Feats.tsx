5be2c99a4c75952b6d23a89d46ae151bfbd9986e
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-12-01 01:42:42 -0300

    Update Feats.tsx

--- src/utils/Feats.tsx
+++ src/utils/Feats.tsx
@@ -2,61 +2,100 @@ export const feats = [
     {
         id: 1,
         name: 'Adepto Elemental',
-    },{
+        description: 'Pré-requisito: Capacidade de conjurar pelo menos uma magia Quando você ganha esse talento, escolha um dos tipos de dano a seguir: ácido, elétrico, fogo, frio ou trovão. As magias que você conjurar ignoram resistência a dano do tipo escolhido. Além disso, quando você rola o dano para uma magia que você conjurar que causar dano desse tipo, você pode tratar qualquer 1 num dado de dano como um 2. Você pode escolher esse talento diversas vezes. A cada vez que o fizer, você deve escolher um tipo diferente de dano'
+    },
+
+    {
         id: 2,
         name: 'Adepto Marcial',
-    },{
+    },
+
+    {
         id: 3,
         name: 'Alerta',
-    },{
+    },
+
+    {
         id: 4,
         name: 'Ambidestro',
-    },{
+    },
+
+    {
         id: 5,
         name: 'Atacante Bestial',
-    },{
+    },
+
+    {
         id: 6,
         name: 'Atirador Aguçado',
-    },{
+    },
+
+    {
         id: 7,
         name: 'Atirador de Magia',
-    },{
+    },
+
+    {
         id: 8,
         name: 'Atleta',
-    },{
+    },
+
+    {
         id: 9,
         name: 'Combatente Montado',
-    },{
+    },
+
+    {
         id: 10,
         name: 'Ator',
-    },{
+    },
+
+    {
         id: 11,
         name: 'Conjurador de Guerra',
-    },{
+    },
+
+    {
         id: 12,
         name: 'Conjurador de Ritual',
-    },{
+    },
+
+    {
         id: 13,
         name: 'Curandeiro',
-    },{
+    },
+
+    {
         id: 14,
         name: 'Duelista Defensivo',
-    },{
+    },
+
+    {
         id: 15,
         name: 'Especialista em Besta',
-    },{
+    },
+
+    {
         id: 16,
         name: 'Especialista em Briga',
-    },{
+    },
+
+    {
         id: 17,
         name: 'Explorador de Cavernas',
-    },{
+    },
+
+    {
         id: 18,
         name: 'Imobilizador',
-    },{
+    },
+
+    {
         id: 19,
         name: 'Iniciado em Magia',
-    },{
+    },
+
+    {
         id: 20,
         name: 'Investida Poderosa',
     },
