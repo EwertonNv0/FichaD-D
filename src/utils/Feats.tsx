804600352d6ee4c5bf9fbf8728f2a3168c5c918c
Ewerton Antonio <100003880+EwertonNv0@users.noreply.github.com>
2022-12-04 13:30:33 -0300

    Update Feats.tsx

--- src/utils/Feats.tsx
+++ src/utils/Feats.tsx
@@ -1,102 +1,127 @@
 export const feats = [
     {
         id: 1,
+        name: 'Nenhum',
+        description: ''
+    },
+
+    {
+        id: 2,
         name: 'Adepto Elemental',
         description: 'Pré-requisito: Capacidade de conjurar pelo menos uma magia Quando você ganha esse talento, escolha um dos tipos de dano a seguir: ácido, elétrico, fogo, frio ou trovão. As magias que você conjurar ignoram resistência a dano do tipo escolhido. Além disso, quando você rola o dano para uma magia que você conjurar que causar dano desse tipo, você pode tratar qualquer 1 num dado de dano como um 2. Você pode escolher esse talento diversas vezes. A cada vez que o fizer, você deve escolher um tipo diferente de dano'
     },
 
     {
-        id: 2,
-        name: 'Adepto Marcial',
-    },
-
-    {
         id: 3,
         name: 'Alerta',
+        description: ''
     },
 
     {
         id: 4,
         name: 'Ambidestro',
+        description: ''
     },
 
     {
         id: 5,
         name: 'Atacante Bestial',
+        description: ''
     },
 
     {
         id: 6,
         name: 'Atirador Aguçado',
+        description: ''
     },
 
     {
         id: 7,
         name: 'Atirador de Magia',
+        description: ''
     },
 
     {
         id: 8,
         name: 'Atleta',
+        description: ''
     },
 
     {
         id: 9,
         name: 'Combatente Montado',
+        description: ''
     },
 
     {
         id: 10,
         name: 'Ator',
+        description: ''
     },
 
     {
         id: 11,
         name: 'Conjurador de Guerra',
+        description: ''
     },
 
     {
         id: 12,
         name: 'Conjurador de Ritual',
+        description: ''
     },
 
     {
         id: 13,
         name: 'Curandeiro',
+        description: ''
     },
 
     {
         id: 14,
         name: 'Duelista Defensivo',
+        description: ''
     },
 
     {
         id: 15,
         name: 'Especialista em Besta',
+        description: ''
     },
 
     {
         id: 16,
         name: 'Especialista em Briga',
+        description: ''
     },
 
     {
         id: 17,
         name: 'Explorador de Cavernas',
+        description: ''
     },
 
     {
         id: 18,
         name: 'Imobilizador',
+        description: ''
     },
 
     {
         id: 19,
         name: 'Iniciado em Magia',
+        description: ''
     },
 
     {
         id: 20,
         name: 'Investida Poderosa',
+        description: ''
+    },
+
+    {
+        id: 21,
+        name: 'Adepto Marcial',
+        description: ''
     },
 ]
